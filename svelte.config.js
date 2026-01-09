52008fde071f6f0d641638813b52100108c55113
Emmet Delaney <emmetdel@gmail.com>
2026-01-09 16:55:12 +0000

    build: switch to svelte-adapter-bun and force bun runtime for build

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "svelte-adapter-bun";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			out: 'build',
-			precompress: true,
-			envPrefix: ''
-		})
-	}
+  kit: {
+    adapter: adapter({
+      out: "build",
+      precompress: true,
+    }),
+  },
 };
 
 export default config;
-
-
